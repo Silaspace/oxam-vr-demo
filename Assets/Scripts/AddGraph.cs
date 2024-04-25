dfdef718d4d488695338891905939eed09caa27a
Silas Harvey <66251036+Silaspace@users.noreply.github.com>
2024-04-25 16:46:35 +0100

    Add graph prefab and new input handler

--- Assets/Scripts/AddGraph.cs
+++ Assets/Scripts/AddGraph.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class AddGraph : MonoBehaviour
 {
     public GameObject tileContainer;
-
     public GameObject tilePrefab;
+    public GameObject graphPrefab;
 
     public void onPress(){
-        Debug.Log("AddGraph.cs :: Instantiate");
+        Debug.Log("AddGraph.cs :: Instantiate a new tile");
         GameObject newTile = Instantiate(
             tilePrefab,
             new Vector3(0, 0, 0),
             Quaternion.identity);
 
+        Debug.Log("AddGraph.cs :: Instantiate a new Graph");
+        GameObject newGraph = Instantiate(
+            graphPrefab,
+            new Vector3(0, 0, 0),
+            Quaternion.identity);
+
         Debug.Log("AddGraph.cs :: Set tile parent");
         newTile.transform.SetParent(tileContainer.transform, false);
+
+        Debug.Log("AddGraph.cs :: Link tile to Graph");
+        newTile.GetComponent<InputHandler>().setGraph(newGraph);
     }
 }
