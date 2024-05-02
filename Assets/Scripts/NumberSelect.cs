dcd495bc27010f35eff89fff5be094f2b212c476
Philanche <philanche@pm.me>
2024-05-02 15:19:10 +0100

    First Attempt at a number selection, to do scale and position

---
+++ Assets/Scripts/NumberSelect.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberSelect : MonoBehaviour
+{
+    public int number;
+    public int min;
+    public int max;
+    public TextMeshProUGUI text;
+
+    public void increment()
+    {
+	if(number < max)
+	{
+	    number++;
+	}
+	else
+	{
+	    number = max;
+	}
+	text.text = number.ToString();
+    }
+
+    public void decrement()
+    {
+	if(number > min)
+	{
+	    number--;
+	}
+	else
+	{
+	    number = min;
+	}
+	text.text = number.ToString();
+    }
+}
